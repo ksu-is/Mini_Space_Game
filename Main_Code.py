8a98a7da3d03f1f57d65308b542f49198d7d4b30
joshvela10 <velasquezjosh35@gmail.com>
2025-11-10 14:54:28 -0500

    Add baseline working application for Sprint2
    This commit includes the current working files for the project during the start of sprint 2. This establishes the baseline code for continued updates throughout the sprint. All files are running locally.

---
+++ Main_Code.py
@@ -0,0 +1,47 @@
+import pygame # Importing pygame for game development
+import random # Importing random module for enemy positions and movements
+import os # Importing os module to load files from assets folder
+
+pygame.init() # This line starts the pygame module
+
+# Setting up the game window dimensions and title
+WIDTH = 800
+HEIGHT = 600
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame. display.set_caption("Space Shooter")
+
+
+#Setting up momving background
+background = pygame.image.load(os.path.join("assets", "background.png")) #Loading background image from assets folder
+background = pygame.transform.scale(background, (WIDTH, HEIGHT)) #Scaling background image to fit game window
+
+bg_y1 = 0 
+bg_y2 = -HEIGHT #Allows two copies of background to move seamlessly
+
+
+# Adding the game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False # Allows you to click "x" to close the game window
+
+
+    #Drawing and scrolling background in game loop
+    screen.blit(background, (0, bg_y1)) 
+    screen.blit(background, (0, bg_y2)) #Drawing scrolling background
+
+
+    bg_y1 += 2
+    bg_y2 += 2 #Moving background downwards
+
+
+    if bg_y1 >= HEIGHT:
+        bg_y1 = -HEIGHT
+    if bg_y2 >= HEIGHT:
+        bg_y2 = -HEIGHT # Creating Scroll loop
+
+    pygame.display.update() #Updating display with new background positions
+pygame.quit()
+
